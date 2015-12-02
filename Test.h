b5c4e373927c3739db173d1f7d2b7abae8d97971
Yamusa Dalhatu <YaMoses@Elyamus-mac.local>
2015-12-03 00:44:08 +0700

    added a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Yamusa Dalhatu on 03/12/15.
+//  Copyright © 2015 Yamusa Dalhatu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Yamusa Dalhatu on 03/12/15.
+//  Copyright © 2015 Yamusa Dalhatu. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
