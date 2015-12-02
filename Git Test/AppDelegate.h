c572ac4280997fd2ebecd77740cffafbefa58d88
Yamusa Dalhatu <YaMoses@elyamus-mac.local>
2015-12-03 00:23:40 +0700

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Yamusa Dalhatu on 03/12/15.
+//  Copyright © 2015 Yamusa Dalhatu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Yamusa Dalhatu on 03/12/15.
+//  Copyright © 2015 Yamusa Dalhatu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
